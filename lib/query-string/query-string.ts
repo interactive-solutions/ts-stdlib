00003170c01d655108a6800faf505a31cd2a19a2
noggan <erik.norgren@interactivesolutions.se>
2015-11-02 11:08:18 +0100

    Added query string parser to queryString util class.

--- lib/query-string/query-string.ts
+++ lib/query-string/query-string.ts
@@ -16,7 +16,7 @@ module is.stdlib {
      * @param obj
      * @returns {string}
      */
-    public static stringify(obj:any) {
+    public static stringify(obj:any): string {
 
       var queryString: string = obj ? Object.keys(obj).sort().map(function (key) {
         var val = obj[key];
@@ -40,6 +40,33 @@ module is.stdlib {
 
       if (queryString.charAt(0) === '&') {
         queryString = queryString.substr(1);
+      }
+
+      return queryString;
+    }
+
+    public static parse():string|boolean {
+      var queryString: any = {};
+      var href: string = window.location.href;
+
+      var query: string = href.split('?')[1];
+
+      if (!query) {
+        return false;
+      }
+
+      var vars: string[]  = query.split('&');
+
+      for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split('=');
+
+        if (typeof queryString[pair[0]] === 'undefined') {
+          queryString[pair[0]] = decodeURIComponent(pair[1]);
+        } else if (typeof queryString[pair[0]] === 'string') {
+          queryString[pair[0]] = [queryString[pair[0]], decodeURIComponent(pair[1])];
+        } else {
+          queryString[pair[0]].push(decodeURIComponent(pair[1]));
+        }
       }
 
       return queryString;
